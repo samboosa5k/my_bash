d8d690343c9886c4b36c832983f0cb04b0bea3e0
Jasper V <jasper.verbon@gmail.com>
2023-09-17 21:36:39 +0200

    [it works]

--- git_change_utils.sh
+++ git_change_utils.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+function conflict_handler() {
+  echo "Checking for conflicts..."
+  local add_or_discard
+  # Check if untracked files exist
+  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+    echo "Untracked files exist"
+    echo "What should we do with untracked files?"
+    echo "1. Add"
+    echo "2. Discard"
+    read -r add_or_discard
+
+    while [ -z "$add_or_discard" ]; do
+      echo "No option provided"
+      echo "What should we do with untracked files?"
+      echo "1. Add"
+      echo "2. Discard"
+      read -r add_or_discard
+    done
+
+    if [ "$add_or_discard" == "1" ]; then
+      echo "Adding and stashing untracked files..."
+      git add .
+    elif [ "$add_or_discard" == "2" ]; then
+      echo "Discarding untracked files..."
+      git clean -f
+    else
+      echo "Invalid option"
+      return 1
+    fi
+  fi
+  return 0
+}
+
+alias conflict_handler=conflict_handler
 
 function git_change_utils() {
   local query
@@ -35,70 +69,41 @@ function git_change_utils() {
     fi
   fi
 
-  local add_or_discard
-  # Check if untracked files exist
-  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-    echo "Untracked files exist"
-    echo "What should we do with untracked files?"
-    echo "1. Add"
-    echo "2. Discard"
-    read -r add_or_discard
-
-    if [ -z "$add_or_discard" ]; then
-      echo "No option provided"
-      return 1
-    else
-      if [ "$add_or_discard" == "1" ]; then
-        echo "Adding and stashing untracked files..."
-        git add .
-      elif [ "$add_or_discard" == "2" ]; then
-        echo "Discarding untracked files..."
-        git clean -f
-      else
-        echo "Invalid option"
-        return 1
-      fi
-    fi
-  fi
+  conflict_handler
 
   local stash_or_commit_or_discard
   # Check if there are changes
   if [ -n "$(git status --porcelain)" ]; then
-    echo "There are uncommitted changes"
-    echo "What should we do with current changes?"
-    echo "1. Commit to current branch"
-    echo "2. Stash then pop in new branch"
-    echo "3. Discard changes"
-    read -r stash_or_commit_or_discard
-
-    if [ -z "$stash_or_commit_or_discard" ]; then
+    echo "Changes found"
+    while [ -z "$stash_or_commit_or_discard" ]; do
       echo "No option provided"
-      return 1
-    fi
+      echo "What should we do with current changes?"
+      echo "1. Commit to current branch"
+      echo "2. Stash then pop in new branch"
+      echo "3. Discard changes"
+      read -r stash_or_commit_or_discard
+    done
 
     if [ "$stash_or_commit_or_discard" == "1" ]; then
       echo "Committing changes to current branch..."
       echo "Enter commit message:"
       read -r commit_message
-      git commit -m "$commit_message" &&
-        git checkout "$target_branch"
+      git commit -m "$commit_message"
+      git checkout "$target_branch"
       return 0
     elif [ "$stash_or_commit_or_discard" == "2" ]; then
       echo "Stashing changes and popping to $target_branch..."
-      git add . &&
-        #        git stash &&
-        git checkout "$target_branch" &&
-        #        git stash pop &&
-        echo "Committing changes to current branch..."
-      echo "Enter commit message:"
-      read -r commit_message
-      git commit -m "$commit_message" &&
-        git checkout "$target_branch"
+      git add .
+      git stash
+      git checkout "$target_branch"
+      conflict_handler
+      git stash pop
+      git add .
       return 0
     elif [ "$stash_or_commit_or_discard" == "3" ]; then
       echo "Discarding changes..."
-      git checkout -- . &&
-        git checkout "$target_branch"
+      git checkout -- .
+      git checkout "$target_branch"
       return 0
     else
       echo "Invalid option"
